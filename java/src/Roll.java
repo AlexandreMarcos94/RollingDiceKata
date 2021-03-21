1e31495972ecdea0fda40842ed4bfcdde820cc66
Alexandre Marcos <alexandremarcos@gmail.com>
2021-03-21 23:30:51 +0100

    All tests passed

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -1,7 +1,4 @@
 package src;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Roll {
 
@@ -17,54 +14,52 @@ public class Roll {
     private int nbRoll;
     private int modifier;
     private Dice unD;
+    private Boolean isN = false;
     private int res = 0;
     private String[] temp;
     private String[] form;
 
     public Roll(String formula) {
 
-        if(formula.matches("^([0-9]+[d][0-9]+)$")){
-            System.out.println("cdc");
-            form = formula.split("d");
-            nbRoll = Integer.parseInt(form[0]);
-            diceValue = Integer.parseInt(form[1]);
-            modifier = 0;
+        if(formula.matches("^([0-9]+[d][0-9]+)$")){         // Check if the formula have the good format
+            form = formula.split("d");                      // Split the formula to get values
+            this.nbRoll = Integer.parseInt(form[0]);
+            this.diceValue = Integer.parseInt(form[1]);
+            this.modifier = 0;
 
             }
 
         if(formula.matches("^([d][0-9]+)$")) {
-            System.out.println("dc");
             form = formula.split("d");
-            nbRoll = 1;
-            modifier = 0;
-            diceValue = Integer.parseInt(form[1]);
+            this.nbRoll = 1;
+            this.modifier = 0;
+            this.diceValue = Integer.parseInt(form[1]);
 
         }
 
         if(formula.matches("^([d][0-9]+([+]|[-])[0-9]+)$")){
-            System.out.println("dc+c");
             form = formula.split("d");
             temp = form[1].split("[+-]");
-            nbRoll = 1;
-            modifier = Integer.parseInt(temp[1]);
-            diceValue = Integer.parseInt(temp[0]);
+            this.nbRoll = 1;
+            this.modifier = Integer.parseInt(temp[1]);
+            this.diceValue = Integer.parseInt(temp[0]);
 
         }
 
         if(formula.matches("^[0-9]+([d][0-9]+([+]|[-])[0-9]+)$")) {
-            System.out.println("cdc+c");
             form = formula.split("d");
-            temp = form[1].split("[+-]");
-            nbRoll = Integer.parseInt(form[0]);
-            modifier = Integer.parseInt(temp[1]);
-            diceValue = Integer.parseInt(temp[0]);
+            if(formula.contains("-")){                          // Verify if the formula content have a - to set the modifier negative
+                isN = Boolean.TRUE;
 
+            }
+            this.temp = form[1].split("[+-]");
+            this.modifier = Integer.parseInt(temp[1]);
+            if(isN){
+                this.modifier = -this.modifier;                 // Set the modifier negative
+            }
+            this.nbRoll = Integer.parseInt(form[0]);
+            this.diceValue = Integer.parseInt(temp[0]);
         }
-
-        System.out.println(diceValue);
-        System.out.println(modifier);
-        System.out.println(nbRoll);
-
     }
 
 
@@ -78,18 +73,17 @@ public class Roll {
     public int makeRoll(RollType rollType) {
 
         this.unD = new Dice( this.diceValue);
-        if( this.diceValue < 1 ||  this.nbRoll < 1 ){
+        if( this.diceValue < 1 ||  this.nbRoll < 1 ){           // Wrong value
             this.res = -1;
         } else {
             for (int i = 0; i <  this.nbRoll; i++) {
-                this.res += this.unD.rollDice();
+                this.res += this.unD.rollDice();                // Throw a dice or multiple dice
             }
             this.res +=  this.modifier;
             if( this.res < -1){
                 this.res = 0;
             }
         }
-        System.out.println("nb" + nbRoll);
         return  this.res;
     }
 
