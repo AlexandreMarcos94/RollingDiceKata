457a7f5db668f2ed96fa7004f5a3cb6b0a35d64a
Alexandre Marcos <alexandremarcos@gmail.com>
2021-03-21 19:46:12 +0100

    5 tests remaining

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -16,9 +16,25 @@ public class Roll {
     private int modifier;
     private Dice unD;
     private int res;
+    private Integer temp;
+    private String[] form;
 
     public Roll(String formula) {
 
+
+        if(formula.matches("/([0-9]?[d][0-9]([+]|[-])?[0-9]?)/")){
+
+            form = formula.split("d");
+            Integer nbRoll = Integer.parseInt(form[0]);
+            Integer dicevalue = Integer.parseInt(form[1]);
+            System.out.print(nbRoll);
+            System.out.print(dicevalue);
+
+        } else {
+
+           System.out.println(formula.matches("/([0-9]?[d][0-9]([+]|[-])?[0-9]?)/"));
+            res = -1;
+        }
 
     }
 
@@ -27,25 +43,32 @@ public class Roll {
     public Roll(int diceValue, int nbRoll, int modifier) {
         unD = new Dice(diceValue);
         diceValue = unD.rollDice();
-        if(diceValue < 1 || nbRoll < 1 || modifier < 0){
+        if(diceValue < 1 || nbRoll < 1 ){
             res = -1;
         } else {
             for (int i = 0; i < nbRoll; i++) {
-
                 res += diceValue;
             }
+            res += modifier;
+            if(res < -1){
+                res = 0;
+            }
         }
+
     }
 
     public int makeRoll(RollType rollType) {
+
         if(rollType == RollType.NORMAL){
             nbRoll = 1;
+
         }
         if(rollType == RollType.ADVANTAGE){
             nbRoll = 2;
         }
         if(rollType == RollType.DISADVANTAGE){
             nbRoll = 2;
+
         }
 
         return res;
